1779d9d2e8c4d376cd61dd8a2c3009b5085f8e06
bernarda <bernarda.sandoval@udla.edu.ec>
2023-06-09 17:57:57 -0500

    corregido

--- src/club/Main.java
+++ src/club/Main.java
@@ -23,7 +23,44 @@ public class Main {
             System.out.println("6. Salir");
             System.out.print("Ingrese una opcion: ");
             op = Integer.parseInt(sc.next());
+            switch (op){
+                case 1:{
+                    System.out.println("Ingrese cedula");
+                    cedula = sc.next();
 
+                    System.out.println("Ingrese nombre");
+                    nombre = sc.next();
+
+                    do{
+                        System.out.println("Ingrese 1 para tipo VIP y 2 para regular");
+                        tipoS = Integer.parseInt(sc.next());
+                    }while(tipoS < 1 || tipoS > 2);
+                    if(tipoS == 1)
+                        tipo = Socio.Tipo.VIP;
+                    else tipo = Socio.Tipo.REGULAR;
+                    c.afiliarSocio(cedula,nombre,tipo);
+
+
+
+                }break;
+                case 2:{
+
+                }break;
+                case 3:{
+
+                }break;
+                case 4:{
+
+                }break;
+                case 5:{
+
+                }break;
+                case 6:{
+                    System.out.println("Gracias!");
+                }break;
+                default:
+                    System.out.println("opcion invalida");
+            }
 
         }while(op!=6);
 
