c0c0a380c8039117c107d65e428687458e8fc21e
bernarda <bernarda.sandoval@udla.edu.ec>
2023-06-09 17:34:28 -0500

    con main

--- src/club/Main.java
+++ src/club/Main.java
@@ -1,19 +1,26 @@
 package club;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner sc = new Scanner(System.in);
+        int op;
+        do{
+            System.out.println("1. Afiliar un socio al club.");
+            System.out.println("2. Registrar una persona autorizada por un socio.");
+            System.out.println("3. Pagar una factura.");
+            System.out.println("4. Registrar un consumo en la cuenta de un socio");
+            System.out.println("5. Aumentar fondos de la cuenta de un socio");
+            System.out.println("6. Salir");
+            System.out.print("Ingrese una opcion: ");
+            op = Integer.parseInt(sc.next());
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        }while(op!=6);
+
+
     }
 }
